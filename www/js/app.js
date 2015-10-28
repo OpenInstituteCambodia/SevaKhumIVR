4ef896c9a3fd336ee2dd036971ec849fd2325d17
Socheat Sok <socheatsok78@gmail.com>
2015-10-28 15:43:13 +0700

    bug fix

--- www/js/app.js
+++ www/js/app.js
@@ -216,6 +216,7 @@ function($scope, $timeout, $state, $ionicHistory, $ionicNavBarDelegate, $datetim
 function($scope, $player, $ionicHistory, $interval) {
 
 	$scope.content_descs = null;
+	$scope.playerPage = null;
 
 	$scope.exit = function() {
 		// navigator.app.exitApp();
